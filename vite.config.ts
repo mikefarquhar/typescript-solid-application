edb0a33702546d2f60d882d3db0cea3c67f90d20
Mike Farquhar <michaelfarquhar1991@gmail.com>
2024-01-16 21:09:21 +0000

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
-// import devTools from "solid-devtools/vite";
 
 export default defineConfig({
-  plugins: [
-    /*
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devTools(),
-    solidPlugin(),
-  ],
+  plugins: [solidPlugin()],
   resolve: {
     alias: [{ find: "~/", replacement: "/src/" }],
   },
