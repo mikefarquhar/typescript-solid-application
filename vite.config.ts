cd4380a7daeefc29734cca001a91a0eb2ee6064b
Mike Farquhar <michaelfarquhar1991@gmail.com>
2024-01-31 21:36:42 +0000

    Replace jest with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,14 @@ export default defineConfig({
   plugins: [solidPlugin()],
   resolve: {
     alias: [{ find: "~/", replacement: "/src/" }],
+    conditions: ["development", "browser"],
   },
   build: {
     target: "esnext",
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["node_modules/@testing-library/jest-dom/vitest"],
   },
 });
